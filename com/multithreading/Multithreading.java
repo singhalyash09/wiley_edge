4cba2c1b310714b084e7865bcd7f1372adefb4ee
Yash Agarwal <agarwalyash493@gmail.com>
2024-04-24 11:19:09 +0530

    Added Multithreading Concept

--- com/multithreading/Multithreading.java
+++ com/multithreading/Multithreading.java
@@ -2,13 +2,22 @@ package com.multithreading;
 
 import java.io.File;
 
-class FileDownloader{
+class FileDownloader extends Thread{
     void download(){
-        System.out.println("Starting Download");
+        System.out.println("Starting Download "+ Thread.currentThread().getName());
         for(int i=1; i<=10; i++){
-            System.out.println(i*10+"% Download Completed");
+            System.out.println(Thread.currentThread().getName()+" : "+i*10+"% Download Completed");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         System.out.println("\nFile Downloaded Successfully\n\n");
+    }
+
+    public void run(){
+        download();
     }
 }
 
@@ -17,9 +26,25 @@ public class Multithreading {
         FileDownloader download1 = new FileDownloader();
         FileDownloader download2 = new FileDownloader();
         FileDownloader download3 = new FileDownloader();
-        download1.download();
-        download2.download();
-        download3.download();
+        long startTime = System.currentTimeMillis();
+        download1.start();
+        download2.start();
+        download3.start();
+
+        download1.setName("Drishyam");
+        download2.setName("3 Idiots");
+        download3.setName("Chhichore");
+
+        try {
+            download1.join();
+            download2.join();
+            download3.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        long endTime = System.currentTimeMillis();
+        long difference = endTime - startTime;
+        System.out.println("Time Taken : "+ difference/1000 + " Seconds");
 
     }
 }
